8fec0092f1cda4e79f8ccf42a1729bf079cee684
561902 <143502140+561902@users.noreply.github.com>
2023-08-31 16:15:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         boubaker ci cd pipeline
+         boubaker ci cd pipeline vv
         </a>
       </header>
     </div>
